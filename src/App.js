7aaedd906060a9ce67b0808973771c80dce1bc86
siriusjunior <topdownreading@gmail.com>
2022-06-11 08:50:19 +0900

    [Add]useStateの例

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const App = () => {
-  return (
-    <div>
-      This is a template for React app.
-    </div>
-  )
-}
+  const [count, setCount] = useState(0);
 
-export default App
+  const increment = () => setCount(count + 1);
+  const decrement = () => setCount(count - 1);
+  const increment2 = () => setCount((previousCount) => previousCount + 1);
+  const decrement2 = () => setCount((previousCount) => previousCount - 1);
+  const reset = () => setCount(0);
+  const double = () => setCount((previousCount) => previousCount * 2);
+  const devide3 = () =>
+    setCount((previousCount) =>
+      previousCount % 3 === 0 ? previousCount / 3 : previousCount
+    );
+
+  return (
+    <>
+      <div>count: {count}</div>
+      <div>
+        <button onClick={increment}>+1</button>
+        <button onClick={decrement}>-1</button>
+      </div>
+      <div>
+        <button onClick={increment2}>+1</button>
+        <button onClick={decrement2}>-1</button>
+      </div>
+      <div>
+        <button onClick={reset}>Reset</button>
+        <button onClick={double}>*2</button>
+        <button onClick={devide3}>3の倍数の時だけ3で割る</button>
+      </div>
+    </>
+  );
+};
+
+export default App;
