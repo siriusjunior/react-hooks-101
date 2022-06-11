bc7553fa3e3b84d060b447b1556074880db4d11c
siriusjunior <topdownreading@gmail.com>
2022-06-11 10:54:47 +0900

    [Add]useEffectによるcallback

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const App = (props) => {
   const [state, setState] = useState(props);
   const { name, price } = state;
+
+  useEffect(() => {
+    console.log("useEffect is invoked.");
+  });
+
+  useEffect(() => {
+    console.log("This is like componentDidMount.");
+  }, []);
+
+  useEffect(() => {
+    console.log("This callback is for name only.");
+  }, [name]);
+
   return (
     <>
       <p>
-        現在の{name}は、{price}円です。
+        現在の{name}は、{price}円です
       </p>
       <button onClick={() => setState({ ...state, price: price + 1 })}>
         +1
